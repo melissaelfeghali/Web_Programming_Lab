2e1479e15ee21a5a28dbb8d1a0fce3ab501b6394
melissa.elfeghali <melissa.elfeghali@lau.edu>
2021-10-15 18:03:51 +0300

    Added CSS + 4 functions JS

--- Lab_6/scriptLab6.js
+++ Lab_6/scriptLab6.js
@@ -4,3 +4,31 @@ function welcome(){
         document.getElementById("greeting").innerHTML = "Welcome " + person;
     }
 }
+
+function add(){
+    let a = document.getElementById("a").value ;
+    let b = document.getElementById("b").value;
+    document.getElementById("result").innerHTML = Number(a) + Number(b) ;
+}
+
+function subtract(){
+    let a = document.getElementById("a").value ;
+    let b = document.getElementById("b").value;
+    document.getElementById("result").innerHTML = Number(a) - Number(b) ;
+}
+
+function multiply(){
+    let a = document.getElementById("a").value ;
+    let b = document.getElementById("b").value;
+    document.getElementById("result").innerHTML = Number(a) * Number(b) ;
+}
+
+function divide(){
+    let a = document.getElementById("a").value ;
+    let b = document.getElementById("b").value;
+    document.getElementById("result").innerHTML = Number(a) / Number(b) ;
+}
+
+function printArray(){
+
+}
