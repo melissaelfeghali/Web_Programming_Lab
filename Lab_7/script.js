4b68bd233c1d24d54d44cbf5735cebbc9b1b7f17
melissa.elfeghali <melissa.elfeghali@lau.edu>
2021-11-04 18:32:18 +0200

    Final Submission

--- Lab_7/script.js
+++ Lab_7/script.js
@@ -12,6 +12,7 @@ class Employee{
         this.address = address;
         this.yof_experience = yof_experience;
         this.salary = 1000;
+        Employee.addEmployee();
     }
 
     print(){
@@ -28,8 +29,47 @@ class Employee{
     }
 }
 
+// Questions 8 to 14
+class Manager extends Employee{
+    static num_manager = 0;
+    static addManager(){
+        Manager.num_manager ++ ;
+    }
 
-// Questions 8 to 16
+    constructor(name, email, age, address, yof_experience, salary, department){
+        super(name, email, age, address, yof_experience);
+        this.salary = 1500;
+        this.department = department;
+        Manager.addManager();
+    }
+
+    print(){
+        super.print();
+        console.log(this.department);
+    }
+
+    raiseSalary(){
+        this.salary = this.salary * 2;
+    }
+}
+
+// Question 15
+const Manager_A = new Manager('Alex Turner', 'alex.tuner@example.com', 45, 'London, United Kingdom', 8);
+const Manager_B = new Manager('Adam Sandler', 'adam_sandler@example.com', 40, 'California, United States', 9);
+
+const Employee_A = new Employee('Michel Saab', 'michel.saab9@example.com', 21, 'Beirut, Lebanon', 1);
+const Employee_B = new Employee('Enzo Marisio', 'enzo.marisio@example.com', 23, 'Milano, Italy', 2);
+const Employee_C = new Employee('Diego Zavala', 'diego.zavala@example.com', 24, 'Cajun, Mexico', 3);
+
+const company_X = [Manager_A, Manager_B, Employee_A, Employee_B, Employee_C];
+
+// Question 16
+function isManager(value, index, array){
+    if(value instanceof Manager){
+        value.raiseSalary();
+    }
+}
+company_X.forEach(isManager);
 
 // Objects
 
